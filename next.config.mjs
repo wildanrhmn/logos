430cf4bfce9ac720ccb76712b2ff02d914de4373
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-06-16 02:04:06 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: () => [
+        {
+            source: "/",
+            destination: "/login",
+            permanent: false
+        },
+    ],
+};
 
 export default nextConfig;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+//TODO//
+export default function Login() {
+    return (
+        <div>
+            <h1 className="">Login</h1>
+        </div>
+    )
+}
